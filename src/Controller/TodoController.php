c9b10868eaf6c92b12c172ac4ca315d6fc035efe
Andrii Shmerchuk <andrii.shmerchuk@moebel.de>
2023-05-04 17:41:23 +0200

    Deleted unused method

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\TodoSqlRepository;
 use App\Service\TodoService;
-use PDO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
--- src/Repository/TodoSqlRepository.php
+++ src/Repository/TodoSqlRepository.php
@@ -39,4 +39,3 @@ class TodoSqlRepository
         $stmt->execute();
     }
 }
-
